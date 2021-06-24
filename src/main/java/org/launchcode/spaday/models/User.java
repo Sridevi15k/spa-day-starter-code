1cbf13d8b9ae20de25d4a63ae0dbabf062af33e5
Sridevi15k <sridevi15k@gmail.com>
2021-06-24 09:52:24 -0500

    6B-Studio: Spa User Validation

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,41 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+
 public class User {
+
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 6)
     private String password;
+
+    /*@NotBlank
+    @Size(min = 6)
+    private String verify;*/
+
+    @NotBlank
+    @NotNull(message = "Passwords do not match")
+    @Size(min = 6)
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password,
+                String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        //this.verify = verify;
+        this.verifyPassword = verifyPassword;
     }
 
     public String getUsername() {
@@ -38,5 +60,29 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    /*public String getVerify() {
+        return verify;
+    }*/
+
+    /*public void setVerify(String verify) {
+        this.verify = verify;
+    }*/
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+    private void checkPassword(){
+        if (password != null && verifyPassword != null &&
+                !password.equals(verifyPassword)) {
+            verifyPassword = null;
+        }
     }
 }
